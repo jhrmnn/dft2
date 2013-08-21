5cf893bb06e46d0f28a32d944c986b1c17a99c55
Jan Hermann <jan@janhermann.cz>
2013-08-21 17:36:02 +0200

    package prepared for easy installation

--- Makefile
+++ Makefile
@@ -1,2 +1,35 @@
-all :
+default :
+	make deploy
+	make install
+	make test
+
+unpack :
+	tar xf deploy.tgz
+
+remove :
+	rm -r data
+	rm dft/getLebedevSphere.m
+	rm -r ints/gen1int
+	rm -r ints/libint
+	rm -r misc/tprod
+
+make install :
+	cd dft
+	make
+	cd ..
+	cd ints
+	cd genint
+	mkdir build
+	cd build
+	cmake -DCMAKE_BUILD_TYPE=Release ..
+	make
+	cd ../..
+	cd libint
+	./configure
+	make
+	cd ..
+	make
+	cd ..
+
+test :
 	matlab -nojvm -r "test; exit"
--- test.m
+++ test.m
@@ -1,9 +1,11 @@
 init();
-%main('h2.xyz', 'cc-pvdz.bas', struct('HF', 1));
+main('h2.xyz', 'cc-pvdz.bas', struct('HF', 1));
 % -1.128708947286
-%main('h2.xyz', 'cc-pvdz.bas', struct('DIRAC', 1, 'PW92C', 1));
+main('h2.xyz', 'cc-pvdz.bas', struct('DIRAC', 1, 'PW92C', 1));
 % -1.131292648499
 %main('ch4-dimer.xyz', 'cc-pvdz.bas', struct('HF', 1));
+% -80.019571818065
 %main('ch4-dimer.xyz', 'cc-pvdz.bas', struct('DIRAC', 1, 'PW92C', 1));
 % -79.807971817104
 %main('ar.xyz', 'cc-pvdz.bas', struct('DENSITY', 1));
+% -18
