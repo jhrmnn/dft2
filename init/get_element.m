deae20f97418d5aca466d303e2a742e2142c2cba
Jan Hermann <jh@janhermann.cz>
2016-02-20 01:32:16 +0100

    rename elements file

--- init/get_element.m
+++ init/get_element.m
@@ -1,7 +1,7 @@
 % converts element symbol on atomic number or vice versa. 12/04/07
 
 function n = get_element(s)
-	table = regexp(fileread('elements.txt'), '\n', 'split');
+	table = regexp(fileread('elements.data'), '\n', 'split');
 	if isnumeric(s)
 		n = table{s};
     else
