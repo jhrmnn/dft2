a3af6f484eab5584aa13f6e778296f1bf0000bca
Jan Hermann <jh@janhermann.cz>
2016-02-20 01:27:48 +0100

    rename basis files

--- test.m
+++ test.m
@@ -1,11 +1,11 @@
 init();
-main('h2.xyz', 'cc-pvdz.bas', struct('HF', 1));
+main('h2.xyz', 'cc-pvdz.data', struct('HF', 1));
 fprintf('Reference: %.15f\n', -1.128708947286);
-main('h2.xyz', 'cc-pvdz.bas', struct('DIRAC', 1, 'PW92C', 1));
+main('h2.xyz', 'cc-pvdz.data', struct('DIRAC', 1, 'PW92C', 1));
 fprintf('Reference: %.15f\n', -1.131292648499);
-%main('ch4-dimer.xyz', 'cc-pvdz.bas', struct('HF', 1));
+%main('ch4-dimer.xyz', 'cc-pvdz.data', struct('HF', 1));
 % -80.019571818065
-%main('ch4-dimer.xyz', 'cc-pvdz.bas', struct('DIRAC', 1, 'PW92C', 1));
+%main('ch4-dimer.xyz', 'cc-pvdz.data', struct('DIRAC', 1, 'PW92C', 1));
 % -79.807971817104
-%main('ar.xyz', 'cc-pvdz.bas', struct('DENSITY', 1));
+%main('ar.xyz', 'cc-pvdz.data', struct('DENSITY', 1));
 % -18
